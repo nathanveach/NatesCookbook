5c17c2e8916e7a12d8fc22639b8e023a2bb0d62a
nathanveach <nathanbveach@gmail.com>
2020-06-11 15:37:15 -0700

    Added recipes component with bootstrap css

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,40 @@
 class Api::V1::RecipesController < ApplicationController
   def index
+  	recipe = Recipe.all.order(created_at: :desc)
+  	render json: recipe
   end
 
   def create
+  	recipe = Recipe.create!(recipe_params)
+  	if recipe
+  		render json: recipe
+  	else
+  		render json: recipe.errors
+  	end
   end
 
   def show
+  	if recipe
+  		render json: recipe
+  	else
+  		render json: recipe.errors
+  	end
   end
 
   def destroy
+  	recipe&.destroy
+  	render json: { message: 'Recipe DESTROYED!' }
   end
+
+
+  private
+
+  def recipe
+  	@recipe ||= Recipe.find(params[:id])
+  end
+  
+  def recipe_params
+  	params.require(:recipe).permit(:name, :image, :ingredients, :instruction)
+  end
+
 end
